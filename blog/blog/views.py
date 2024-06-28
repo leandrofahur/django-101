5d393ca1bd81107f92e5fa51c7795b2d1cdde043
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-06-28 15:30:38 -0700

    restful framework

---
+++ blog/blog/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'message': 'Welcome to the API!',
+        'info': 'This is a simple API endpoint.'
+    })
+
+@api_view(['GET'])
+def api_posts(request, format=None):
+    posts = [
+        {'id': 1, 'title': 'Post 1', 'content': 'Content of post 1'},
+        {'id': 2, 'title': 'Post 2', 'content': 'Content of post 2'},
+    ]
+    return Response(posts)
+
