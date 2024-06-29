ef6434086823a0b25738cf5dd0252ab6a61737fe
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-06-28 19:25:05 -0700

    updated

--- blog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'rating', 'author', 'is_liked')
-    search_fields = ('title', 'author')
-    list_filter = ('rating', 'is_liked')
